8050b20935789a45576c183b76c19e05c3e9853d
DavidJGR <davidg3912@gmail.com>
2022-07-20 21:21:58 -0700

    Autocommit from 2022-07-20 21:21:58

--- scripts/gitbackup.sh
+++ scripts/gitbackup.sh
@@ -7,6 +7,18 @@
 ### Path to your config folder you want to back up
 config_folder=~/klipper_config
 
+### Path to your Klipper folder, by default that is '~/klipper'
+klipper_folder=~/klipper
+
+### Path to your Moonraker folder, by default that is '~/moonraker'
+moonraker_folder=~/moonraker
+
+### Path to your Mainsail folder, by default that is '~/mainsail'
+mainsail_folder=~/mainsail
+
+### Path to your Fluidd folder, by default that is '~/fluidd'
+#fluidd_folder=~/fluidd
+
 #####################################################################
 #####################################################################
 
@@ -23,7 +35,20 @@ grab_version() {
     klipper_commit=$(git rev-parse --short=7 HEAD)
     m1="Klipper on commit: ${klipper_commit}"
   fi
- }
+  if [[ -n ${moonraker_folder} ]]; then
+    cd "${moonraker_folder}"
+    moonraker_commit=$(git rev-parse --short=7 HEAD)
+    m2="Moonraker on commit: ${moonraker_commit}"
+  fi
+  if [[ -n ${mainsail_folder} ]]; then
+    mainsail_ver=$(head -n 1 "${mainsail_folder}/.version")
+    m3="Mainsail version: ${mainsail_ver}"
+  fi
+  if [[ -n ${fluidd_folder} ]]; then
+    fluidd_ver=$(head -n 1 "${fluidd_folder}/.version")
+    m4="Fluidd version: ${fluidd_ver}"
+  fi
+}
 
 push_config() {
   local current_date
